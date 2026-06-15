fc5a5f2a76f920316c5bffb05fc61260869bde5b
Vishwapriya M <vishwapriyam58@gmail.com>
2026-06-15 16:56:49 +0530

    Added Day 6 Logistic Regression

---
+++ day05/day06_logistic_regression.py
@@ -0,0 +1,18 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+X = [[10], [20], [30], [40], [50], [60]]
+y = [0, 0, 1, 1, 1, 1]
+
+ 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+ 
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+ 
+y_pred = model.predict(X_test)
+ 
+print("Accuracy:", accuracy_score(y_test, y_pred))
